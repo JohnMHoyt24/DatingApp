00efdcd31f877641f61ae345c92c0b58b528d589
John Hoyt <Jmhoyt6355@gmail.com>
2020-10-29 11:40:16 -0400

    Updated Account Controller & added DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,40 +1,49 @@
 //using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-//using System.Linq;
+using System.Linq;
 using System.Text;
 using System.Security.Cryptography;
 using API.Data;
+using API.DTOs;
 //using API.Data.Migrations.DataContext;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 //using static API.Controllers.BaseApiController;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        //DataContext context = DataContext();
+        private readonly DataContext _context;
         public AccountController(DataContext context){
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+           
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-                PasswordSalt = hmac.Key()
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
     }
