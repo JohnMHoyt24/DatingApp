dea4f0909f768857fed4c29099adeab94001324c
John Hoyt <Jmhoyt6355@gmail.com>
2020-11-06 19:00:20 -0500

    "Updated registration and added login"

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,6 +42,25 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>>Login(LoginDto loginDto){
+            var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(Login.Password));
+
+            for(int i = 0; i < ComputeHash.Length; i++){
+                if(ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username){
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
